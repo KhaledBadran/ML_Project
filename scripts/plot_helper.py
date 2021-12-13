b9b983fafc4dcad3aea3e9ef0782adc47f1629f5
Sharon Ho <33813045+sharon-ho@users.noreply.github.com>
2021-12-12 21:52:49 -0500

    Update plot_helper.py

--- scripts/plot_helper.py
+++ scripts/plot_helper.py
@@ -18,13 +18,13 @@ def plot_bar(data_dict):
         plt.text(x_bar1[index], value+0.01, "{0:.3f}".format(value))
     
     recall = [score[1] for score in scores]
-    bar2 = plt.bar(r + width, recall, width, color=colors[1], edgecolor='black')
+    bar2 = plt.bar(r+width, recall, width, color=colors[1], edgecolor='black')
     x_bar2 = r+width-0.1
     for index, value in enumerate(recall):
         plt.text(x_bar2[index], value+0.01, "{0:.3f}".format(value))
     
     f1 = [score[2] for score in scores]
-    bar3 = plt.bar(r + width*2, f1, width, color=colors[2], edgecolor='black')
+    bar3 = plt.bar(r+width*2, f1, width, color=colors[2], edgecolor='black')
     x_bar3 = r+width*2-0.1
     for index, value in enumerate(f1):
         plt.text(x_bar3[index], value+0.01, "{0:.3f}".format(value))
@@ -32,6 +32,6 @@ def plot_bar(data_dict):
     plt.title("Results")
     plt.xlabel("Entity Type")
     plt.ylim(0.0,1.0)
-    plt.xticks(r + width, n_entities)
+    plt.xticks(r+width, n_entities)
     plt.legend((bar1, bar2, bar3), ('Precision', 'Recall', 'F1'))
     plt.show()
